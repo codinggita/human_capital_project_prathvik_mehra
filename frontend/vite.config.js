af27a6b108971bfb9fbebf5ccb3df8cf56bbb8ae
prathvik mehra <prathvik.mehra.cg@gmail.com>
2026-06-18 16:55:02 +0530

    chore(frontend): update package dependencies, vite config, and index template

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,10 +2,30 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss()
+    tailwindcss(),
   ],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('@mui') || id.includes('@emotion')) {
+              return 'mui';
+            }
+            if (id.includes('recharts') || id.includes('framer-motion')) {
+              return 'recharts';
+            }
+            if (id.includes('react') || id.includes('redux')) {
+              return 'vendor';
+            }
+          }
+        }
+      }
+    }
+  }
 })
